db33aed79e89f59f408eeffd90e95c00cfb144a5
velund <velundkvz@yahoo.com>
2020-06-08 18:08:54 +0200

    timer added

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ window.addEventListener('DOMContentLoaded',function(){
         TabsContents = document.querySelectorAll('.info-tabcontent'),
         TabsWrapper = document.querySelector('.info-header');
     toggleTabs(Tabs, TabsContents, TabsWrapper);
-    
+    //setClock('timer', deadline);
     
 
 });
+
+// Tabs toggler function
 
 function toggleTabs(tabs, contents, tabsWrapper){
     function hideTabConent (a){
@@ -37,4 +39,48 @@ function toggleTabs(tabs, contents, tabsWrapper){
             
         }
     });
+}
+
+// Timer function
+
+let deadline = '2020-06-09';
+
+function calcTime(endtime){
+    let milsec = Date.parse(endtime) - Date.parse(new Date()),
+        secs = 0, mins = 0, hours = 0, days = 0;
+    if (milsec>0){
+        secs = (Math.floor(milsec/1000))%60;
+        mins = (Math.floor(milsec/(1000*60)))%60;
+        hours = (Math.floor(milsec/(1000*60*60))%24);
+        days = (Math.floor(milsec/(1000*60*60*24)));
+    }    
+    let timeData  = {
+        milliseconds: milsec,
+        seconds: secs,
+        minutes: mins,
+        hours: hours,
+        days: days
+    }
+    return timeData;
+
+}
+console.log(calcTime(deadline).hours);
+function setClock(id, endtime){
+    let t = document.getElementById(id),
+        seconds = t.querySelector('.seconds'),
+        minutes = t.querySelector('.minutes'),
+        hours = t.querySelector('.hours'),
+        days = t.querySelector('.days');
+    let timeInterval = setInterval(updateClock, 1000);
+    function updateClock(){
+        seconds.textContent = addZero(calcTime(endtime).seconds);
+        minutes.textContent = addZero(calcTime(endtime).minutes);
+        hours.textContent = addZero(calcTime(endtime).hours);
+        days.textContent = addZero(calcTime(endtime).days);
+    }
+    function addZero(num){
+        if (num<10) {
+            return ('0'+num);
+        }else {return num;}
+    }
 }
